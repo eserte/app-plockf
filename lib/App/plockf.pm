e283212f173236625ae834e176abeb99b1435aa8
Slaven Rezic <slaven@rezic.de>
2017-03-19 22:27:22 +0100

    prepare 0.04

--- lib/App/plockf.pm
+++ lib/App/plockf.pm
@@ -3,7 +3,7 @@
 #
 # Author: Slaven Rezic
 #
-# Copyright (C) 2016 Slaven Rezic. All rights reserved.
+# Copyright (C) 2016,2017 Slaven Rezic. All rights reserved.
 # This package is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -15,7 +15,7 @@ package App::plockf;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.03';
+$VERSION = '0.04';
 
 1;
 
